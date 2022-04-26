a4f344490f51f08e63228f7c060af3a31c27f280
Donghyun Kim <95532855+kimdhj@users.noreply.github.com>
2022-04-26 17:59:01 +0900

    baekjoon practice 3052

---
+++ Practice/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		for(int i = 0; i < 10; i ++) {
+			int a = Integer.parseInt(br.readLine());
+			arr.add(a % 42);
+		}//for
+		
+		Set<Integer> set = new HashSet<>(arr);
+		System.out.println(set.size());
+		
+		
+		
+	}
+	
+}

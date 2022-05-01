6b54d82234dc7249e9e96cb87c086930733215ea
Donghyun Kim <95532855+kimdhj@users.noreply.github.com>
2022-05-02 00:43:19 +0900

    Update Main2908.java

--- Practice/src/Main2908.java
+++ Practice/src/Main2908.java
@@ -4,32 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main2908 {
-
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int cnt = st.countTokens();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int[] numCheckArr = new int[2];
 		int max = 0;
-		int[] numArr = new int[2];//변환한 숫자 받을 배열
-		
-		for(int i = 0; i < cnt; i++) {
-			char tmp = '\u0000';
-			String str = st.nextToken();
-			char[] charArr = str.toCharArray();//받은 문자를 char배열로 변환
-			tmp = charArr[0];
-			charArr[0] = charArr[2];
-			charArr[2] = tmp;
-			String newStr = new String(charArr);
-			numArr[i] = Integer.parseInt(newStr);
+		for(int i = 0; i < numCheckArr.length; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			int sangSuNum = (num % 10) * 100 + ((num % 100) - (num % 10)) + (num / 100);
+			numCheckArr[i] = sangSuNum;
 		}
-		
-		if(numArr[0] > numArr[1]) {
-			max = numArr[0];
+		if(numCheckArr[0] > numCheckArr[1]) {
+			max = numCheckArr[0];
 		} else {
-			max = numArr[1];
+			max = numCheckArr[1];
 		}
 		System.out.println(max);
+		br.close();
 	}
 	
 }

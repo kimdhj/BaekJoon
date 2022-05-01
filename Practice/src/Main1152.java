774c2c504958bd688a48efeee3e6bcbb1b1c48c4
Donghyun Kim <95532855+kimdhj@users.noreply.github.com>
2022-05-02 02:35:39 +0900

    Update Main1152.java

--- Practice/src/Main1152.java
+++ Practice/src/Main1152.java
@@ -1,15 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main1152 {
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int cnt = st.countTokens();
+		
+		String str = br.readLine();
+		char preChar = 32;
+		char curChar = '\u0000';
+		int cnt = 0;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == 32 || str.charAt(i) == 10) {
+				preChar = 32;
+				continue;
+			} else {
+				curChar = str.charAt(i);
+				if(preChar == 32 && curChar != 32) {
+					preChar = '\u0000';
+					cnt++;
+				}
+				
+			}//if else
+		}//for
 		System.out.println(cnt);
 				
 	}

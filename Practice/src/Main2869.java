d89e1009a7345767726dfe76b5e65b42c238899c
Donghyun Kim <95532855+kimdhj@users.noreply.github.com>
2022-05-06 23:57:14 +0900

    Update Main2869.java

--- Practice/src/Main2869.java
+++ Practice/src/Main2869.java
@@ -3,23 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main2869 {
-
-	public static void main(String[] args) throws IOException {
-
+class Main2869 {
+	
+	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		int v = Integer.parseInt(st.nextToken());
-		int day = 0;
 		
+		int day = 0;
 		day = (v - b) / (a - b);
 		if((v - b) % (a - b) != 0) {
-			day += 1;
+			day++;
 		}
 		System.out.println(day);
 	}
-
+	
 }

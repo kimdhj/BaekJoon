455c6c37632670832ce95af2c3d56636ac797d66
Donghyun Kim <hyunhahaha13@gmail.com>
2022-10-13 22:02:06 +0900

    Create Main10814.java
    Baekjoon Practice 10814

---
+++ Practice/src/arrange/Main10814.java
@@ -0,0 +1,44 @@
+package arrange;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main10814 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		String[][] userArr = new String[N][3];
+		StringTokenizer st;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			userArr[i][0] = st.nextToken();
+			userArr[i][1] = st.nextToken();
+			userArr[i][2] = String.valueOf(i);
+		}
+		
+		Arrays.sort(userArr, new Comparator<String[]>() {
+
+			@Override
+			public int compare(String[] o1, String[] o2) {
+				if(o1[0] == o2[0]) {
+					return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
+				} else {
+					return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+				}
+				
+			}
+			
+		});
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			sb.append(userArr[i][0] + " " + userArr[i][1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}

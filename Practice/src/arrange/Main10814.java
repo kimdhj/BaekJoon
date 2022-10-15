50265c0ea5bb37e13eef23ead642581febea070f
Donghyun Kim <hyunhahaha13@gmail.com>
2022-10-15 15:11:59 +0900

    Update Main10814.java

--- Practice/src/arrange/Main10814.java
+++ Practice/src/arrange/Main10814.java
@@ -12,14 +12,14 @@ public class Main10814 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		String[][] userArr = new String[N][3];
+		String[][] userArr = new String[N][3]; //나이, 이름, 가입순서 입력받을 2차원배열 생성
 		StringTokenizer st;
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			userArr[i][0] = st.nextToken();
 			userArr[i][1] = st.nextToken();
-			userArr[i][2] = String.valueOf(i);
+			userArr[i][2] = String.valueOf(i);//가입한순서
 		}
 		
 		Arrays.sort(userArr, new Comparator<String[]>() {
@@ -28,6 +28,7 @@ public class Main10814 {
 			public int compare(String[] o1, String[] o2) {
 				if(o1[0] == o2[0]) {
 					return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
+					//return Integer.parseInt(o2[2]) - Integer.parseInt(o1[2]); 나이가 같을경우 내림차순
 				} else {
 					return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
 				}

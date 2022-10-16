86cb9e7b57cd7b70ded804f2b625e9c579a962b9
Donghyun Kim <hyunhahaha13@gmail.com>
2022-10-16 18:12:32 +0900

    Create Main14425.java

---
+++ Practice/src/setmap/Main14425.java
@@ -0,0 +1,31 @@
+package setmap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main14425 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		Set<String> set = new HashSet<>();
+		for(int i = 0; i < N; i++) {
+			set.add(br.readLine());
+		}
+		
+		int cnt = 0;
+		for(int i = 0; i < M; i++) {
+			String word = br.readLine();
+			if(set.contains(word)) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
